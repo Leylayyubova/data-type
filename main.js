0e1a541525c04df5f6d70653aaac68ddcb3789a9
Leyla Ayyubova <102313083+Leylayyubova@users.noreply.github.com>
2024-04-30 22:26:06 +0400

    data type

---
+++ main.js
@@ -0,0 +1,35 @@
+let text = 'Hellush World!';
+console.log(text.length);
+
+let x = 10;
+let y= 5;
+console.log(x * y);
+
+let a = 16;
+let b = 5;
+let c = 13;
+let s = (a + b + c)/2;
+
+if (s * (s - a) * (s - b) * (s - c) < 0) {
+  console.log('Deyer menfidir.');
+} else {
+  let heron = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+  console.log(heron);
+}
+
+// // let x = prompt('eded daxil edin');
+// // if (x>100) {
+// //   alert('true');
+// // } else{
+// //   alert('false');
+// // }
+// let element = prompt ('Sheher daxil edin');
+// if (element.toLocaleLowerCase() == "Baki".toLocaleLowerCase()) {
+//   console.log("bakiya catdirilma movcuddur");
+// }
+// else {
+//   console.log("bakiya catdirilma yoxdur");
+// }
+
+
+
